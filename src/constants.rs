b09f440c66426007af0146f3b2d5379eed2f334a
Tim <tymmesyde@gmail.com>
2026-01-11 21:18:09 +0100

    refactor: use static server version

--- src/constants.rs
+++ src/constants.rs
@@ -5,8 +5,8 @@ pub const ITEM_MAX_SIZE: i32 = 225;
 pub const ITEM_MIN_SIZE: i32 = (ITEM_MAX_SIZE as f64 * POSTER_RATIO) as i32;
 pub const CATALOG_ICON_SIZE: i32 = 24;
 pub const DETAILS_LOGO_SIZE: (i32, i32) = (80, 160);
-pub const SERVER_UPDATER_ENDPOINT: &str = "https://www.strem.io/updater/server/check";
 pub const SERVER_DOWNLOAD_ENDPOINT: &str = "https://dl.strem.io/server/vVERSION/desktop/server.js";
+pub const SERVER_VERSION: &str = "4.20.16";
 pub const COMMUNITY_MANIFESTS: &[&str] = &[
     "https://v3-cinemeta.strem.io/manifest.json",
     "https://stremio-addons.com/manifest.json",
--- src/server.rs
+++ src/server.rs
@@ -5,34 +5,21 @@ use std::{
 };
 
 use anyhow::Context;
-use serde::Deserialize;
 use url::Url;
 
-use crate::constants::{SERVER_DOWNLOAD_ENDPOINT, SERVER_UPDATER_ENDPOINT};
-
-#[derive(Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct ServerUpdaterResponse {
-    latest_version: String,
-}
+use crate::constants::{SERVER_DOWNLOAD_ENDPOINT, SERVER_VERSION};
 
 pub async fn initialize(data_location: &Path) -> anyhow::Result<Child> {
     let file_path = Path::new(&data_location).join("server.js");
     let version_path = Path::new(&data_location).join("server_version");
 
-    let latest_version = reqwest::get(SERVER_UPDATER_ENDPOINT)
-        .await?
-        .json::<ServerUpdaterResponse>()
-        .await?
-        .latest_version;
-
     let should_download = fs::read_to_string(&version_path)
-        .map_or(true, |current_version| current_version != latest_version);
+        .map_or(true, |current_version| current_version != SERVER_VERSION);
 
     if should_download {
         let download_url = Url::parse(
             SERVER_DOWNLOAD_ENDPOINT
-                .replace("VERSION", &latest_version)
+                .replace("VERSION", &SERVER_VERSION)
                 .as_str(),
         )?;
 
@@ -43,7 +30,7 @@ pub async fn initialize(data_location: &Path) -> anyhow::Result<Child> {
             .context("Failed to fetch server file")?;
 
         fs::write(&file_path, latest_file).context("Failed to write server file")?;
-        fs::write(&version_path, &latest_version).context("Failed to write version file")?;
+        fs::write(&version_path, &SERVER_VERSION).context("Failed to write version file")?;
     }
 
     let process = Command::new("node")
